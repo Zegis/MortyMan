2ca45f23fa71fbb04cc862cae4793c78e0d96cc1
Zegis <kjgm@poczta.onet.pl>
2015-04-24 20:01:06 +0200

    Added pills layer

--- js/morty.js
+++ js/morty.js
@@ -1,9 +1,10 @@
-var game = new Phaser.Game(28 * 32,31 * 32, Phaser.AUTO, '', { preload: preload, create: create, update: update });
-var map, mapLayer;
+var game = new Phaser.Game(28 * 28,31 * 28, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var map, mapLayer, pillsLayer;
 
 function preload(){
 
 	game.load.image("tileset","assets/tilemap.png");
+	game.load.image("pills","assets/pills.png");
 	game.load.tilemap("map","map/tmap.json",null,Phaser.Tilemap.TILED_JSON);
 
 };
@@ -12,9 +13,12 @@ function create(){
 
 	map = game.add.tilemap("map");
 	map.addTilesetImage("tilemap","tileset");
+	map.addTilesetImage("pills","pills");
 	
 	mapLayer = map.createLayer("tiles");
+	pillsLayer = map.createLayer("pills");
 	mapLayer.resizeWorld();
+
 
 };
 
