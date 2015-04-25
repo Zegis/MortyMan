3975183a7de9e0065b9323a1ba14463c49115eb5
Zegis <kjgm@poczta.onet.pl>
2015-04-25 18:09:41 +0200

    Small refactor

--- js/morty.js
+++ js/morty.js
@@ -13,23 +13,9 @@ function preload(){
 };
 
 function create(){
-
-	map = game.add.tilemap("map");
-	map.addTilesetImage("tilemap","tilemap");
-	map.addTilesetImage("pills","pills");
 	
-	
-	
-	mapLayer = map.createLayer("tiles");
-	pillsLayer = map.createLayer("pills");
-	
-	mapLayer.resizeWorld();
-	
-	// create player
-	player = game.add.sprite(28*13 , 28 * 23,"pacman");
-	game.physics.enable(player, Phaser.Physics.ARCADE);
-	
-	map.setCollisionByExclusion([1],true, "tiles");
+	createMap();
+	createPlayer();
 	
 	// create controls
 	controls = game.input.keyboard.createCursorKeys();
@@ -37,18 +23,37 @@ function create(){
 
 };
 
+function createMap(){
+	map = game.add.tilemap("map");
+	
+	map.addTilesetImage("tilemap","tilemap");
+	map.addTilesetImage("pills","pills");
+	
+	mapLayer = map.createLayer("tiles");
+	pillsLayer = map.createLayer("pills");
+	mapLayer.resizeWorld();
+	
+	map.setCollisionByExclusion([1],true, "tiles");
+};
+
+function createPlayer(){
+	player = game.add.sprite((28*13)+16 , (28 * 23)+1,"pacman");
+	game.physics.enable(player, Phaser.Physics.ARCADE);
+}
+
 function update(){
 
 	this.game.physics.arcade.collide(player,mapLayer);
 
 	player.body.velocity.x = 0;
 	player.body.velocity.y = 0;
+	
 	if(controls.left.isDown)
-		player.body.velocity.x = -128;
+		player.body.velocity.x = -112;
 	else if(controls.right.isDown)
-		player.body.velocity.x = 128;
-	else if(controls.down.isDown)
-		player.body.velocity.y = 128;
+		player.body.velocity.x = 112;
+	if(controls.down.isDown)
+		player.body.velocity.y = 112;
 	else if(controls.up.isDown)
-		player.body.velocity.y = -128;
+		player.body.velocity.y = -112;
 };
