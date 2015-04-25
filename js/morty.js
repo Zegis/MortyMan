559c9f32c992ff62b36fe7157ddd562c27b2a36d
Zegis <kjgm@poczta.onet.pl>
2015-04-25 22:18:21 +0200

    Pacman can eat small pills

--- js/morty.js
+++ js/morty.js
@@ -35,20 +35,28 @@ function createMap(){
 	map.setCollisionByExclusion([1],true, "tiles");
 	
 	scorePills = game.add.group();
+	game.physics.enable(scorePills, Phaser.Physics.ARCADE);
+	scorePills.enableBody = true;
 	superPills = game.add.group();
 	
 	map.createFromTiles(6,null,"pills","pills",scorePills, {frame: 0});
 	map.createFromTiles(7,null,"pills","pills",superPills, {frame: 1});
+	
+	scorePills.forEach(function(s){
+		s.body.immovable = true;
+	},this);
 };
 
 function createPlayer(){
 	player = game.add.sprite((28*13)+16 , (28 * 23)+1,"pacman");
 	game.physics.enable(player, Phaser.Physics.ARCADE);
+	
 }
 
 function update(){
 
 	this.game.physics.arcade.collide(player,mapLayer);
+	this.game.physics.arcade.collide(player,scorePills,score);
 
 	player.body.velocity.x = 0;
 	player.body.velocity.y = 0;
@@ -61,4 +69,14 @@ function update(){
 		player.body.velocity.y = 112;
 	else if(controls.up.isDown)
 		player.body.velocity.y = -112;
+		
+	if(scorePills.countDead() == scorePills.length)
+		alert("Victory!");
+};
+
+function score(player,pill){
+	console.log("!");
+	pill.kill();
+	
+	console.log(scorePills.countDead());
 };
