0d765f652fbe8fa81e72a8a771429b333be96208
Zegis <kjgm@poczta.onet.pl>
2015-06-11 03:26:31 +0200

    Latest public build

--- js/morty.js
+++ js/morty.js
@@ -31,10 +31,12 @@ function create(){
 	createMap();
 	createPlayer();
 	
-	blinky = game.add.sprite(28*2, (28*5)+1,"blinky");
+	blinky = game.add.sprite((28*1), (28*2),"blinky");
+//	blinky.anchor.set(0.5);
 	game.physics.enable(blinky, Phaser.Physics.ARCADE);
 	blinky.body.bounce.y = 0;
 	blinky.body.bounce.x = 0;
+	
 	
 	// create controls
 	controls = game.input.keyboard.createCursorKeys();
@@ -45,18 +47,18 @@ function create(){
 	superTimer = game.time.create(false);
 	
 	marker = new Phaser.Point(0,0);
-	target = new Phaser.Point(9,13);
+	target = new Phaser.Point(1,29);
 	directions = [null, null, null, null, null];
 	distance = [null, null, null, null, null];
 	
-	blinky.body.velocity.x = 100;
+	blinky.body.velocity.x = 93;
 	blinky.direction = 2;
 	
 	decisionPoints = [ new Phaser.Point(6,1), new Phaser.Point(21,1),
 	new Phaser.Point(1,5),new Phaser.Point(6,5),new Phaser.Point(9,5),new Phaser.Point(12,5),
 	new Phaser.Point(15,5),new Phaser.Point(18,5),new Phaser.Point(21,5),new Phaser.Point(26,5),
 	new Phaser.Point(6,8),new Phaser.Point(21,8),
-	new Phaser.Point(6,14),new Phaser.Point(9,14),new Phaser.Point(18,14),new Phaser.Point(24,14),
+	new Phaser.Point(6,14),new Phaser.Point(9,14),new Phaser.Point(18,14),new Phaser.Point(21,14),
 	new Phaser.Point(9,17),new Phaser.Point(18,17),
 	new Phaser.Point(6,20),new Phaser.Point(9,20),new Phaser.Point(18,20),new Phaser.Point(21,20),
 	new Phaser.Point(6,23),new Phaser.Point(9,23),new Phaser.Point(18,23),new Phaser.Point(21,23),
@@ -93,7 +95,7 @@ function createMap(){
 
 function createPlayer(){
 	//player = game.add.sprite((28*13)+16 , (28 * 23)+1,"pacman"); // original start point
-	player = game.add.sprite((28*3) , (28 * 14)+1,"pacman"); // debug start point
+	player = game.add.sprite((28*3) , (28 * 14),"pacman"); // debug start point
 	game.physics.enable(player, Phaser.Physics.ARCADE);
 	
 }
@@ -105,46 +107,66 @@ function update(){
 	this.game.physics.arcade.overlap(player,superPills, makeSuper);
 	this.game.physics.arcade.overlap(player,blinky, touchGhost);
 	
-	marker.x = Phaser.Math.snapToFloor(Math.floor(blinky.x),28) / 28;
-	marker.y = Phaser.Math.snapToFloor(Math.floor(blinky.y),28) / 28;
+	var	tmpX ;
+	var	tmpY;
 	
-	game.debug.geom(new Phaser.Rectangle(target.x*28, target.y*28, 28, 28), "#CCFFCC", true);
-	if(decisionPoints.contains(marker)) // if in decision point
+	if(blinky.body.velocity.x < 0)
+		tmpX = Phaser.Math.snapToFloor(Math.floor(blinky.x + blinky.width),28) / 28;
+	else
+		tmpX = Phaser.Math.snapToFloor(Math.floor(blinky.x),28) / 28;
+		
+	if(blinky.body.velocity.y < 0)
 	{
-		game.debug.text("true",20,20, "#CCC");
-		decideGhostDirection();
+		tmpY  = Phaser.Math.snapToFloor(Math.floor(blinky.y + blinky.height),28) / 28;
 	}
 	else
-	{
-		game.debug.text("false",20,20, "#CCC");
+		tmpY  = Phaser.Math.snapToFloor(Math.floor(blinky.y),28) / 28;
+	
+	game.debug.geom(new Phaser.Rectangle(target.x*28, target.y*28, 28, 28), "#CCFFCC", true);
+	//game.debug.geom(new Phaser.Rectangle(tmpX*28, tmpY*28, 28, 28), "#CCCCFF", true);
+	// game.debug.text(test,20, 20, "#CCC");
+	if(tmpX != marker.x || tmpY != marker.y){
+		marker.x = tmpX;
+		marker.y = tmpY;
+		if(decisionPoints.contains(marker)) // if in decision point
+		{
+			decideGhostDirection();
+		}
 	}
-		this.game.physics.arcade.collide(blinky,mapLayer, ghostCollide);
+
+	this.game.physics.arcade.collide(blinky,mapLayer, ghostCollide);
+	
 	
 	// check Key
 	if(controls.left.isDown)
 	{
-		player.body.velocity.x = -112;
+		player.body.velocity.x = -93;
 		current = Phaser.LEFT;
 	}
 	else if(controls.right.isDown)
 	{
-		player.body.velocity.x = 112;
+		player.body.velocity.x = 93;
 		current = Phaser.RIGHT;
 	}
 	if(controls.down.isDown)
 	{
-		player.body.velocity.y = 112;
+		player.body.velocity.y = 93;
 		current = Phaser.DOWN;
 	}
 	else if(controls.up.isDown)
 	{
-		player.body.velocity.y = -112;
+		player.body.velocity.y = -93;
 		current = Phaser.UP;
 	}
 	
 	if(scorePills.countDead() == scorePills.length)
 		alert("Victory!");
 	
+	if(game.input.activePointer.isDown)
+	{
+		target.x = Phaser.Math.snapToFloor(Math.floor(game.input.x),28) / 28;
+		target.y = Phaser.Math.snapToFloor(Math.floor(game.input.y),28) / 28;
+	}
 	
 		
 	if( (player.x) >= (28*28+14))
@@ -184,6 +206,7 @@ function touchGhost(player, ghost){
 
 function ghostCollide(ghost, tile)
 {
+	
 	directions[1] = map.getTileAbove(mapLayer.index, marker.x, marker.y);
 	directions[2] = map.getTileLeft(mapLayer.index, marker.x, marker.y);
 	directions[3] = map.getTileBelow(mapLayer.index, marker.x, marker.y);
@@ -195,23 +218,23 @@ function ghostCollide(ghost, tile)
 		{
 			if(i === 1)
 			{
-				ghost.body.velocity.y = -100;
+				ghost.body.velocity.y = -93;
 				ghost.direction = 3;
 			}
 			else if(i === 2)
 			{
 				ghost.direction = 4;
-				ghost.body.velocity.x = -100;
+				ghost.body.velocity.x = -93;
 			}
 			else if(i === 3)
 			{
 				ghost.direction = 1;
-				ghost.body.velocity.y = 100;
+				ghost.body.velocity.y = 93;
 			}
 			else if(i === 4)
 			{
 				ghost.direction = 2;
-				ghost.body.velocity.x = 100;
+				ghost.body.velocity.x = 93;
 			}
 				
 			break;
@@ -223,49 +246,60 @@ function ghostCollide(ghost, tile)
 	}
 }
 
-function decideGhostDirection(){
-
+function decideGhostDirection()
+{
 	directions[1] = map.getTileAbove(mapLayer.index, marker.x, marker.y);
 	directions[2] = map.getTileLeft(mapLayer.index, marker.x, marker.y);
 	directions[3] = map.getTileBelow(mapLayer.index, marker.x, marker.y);
 	directions[4] = map.getTileRight(mapLayer.index, marker.x, marker.y);
 	
+	blinky.x = marker.x*28;
+	blinky.y = marker.y*28;
+	
+	blinky.body.reset(marker.x*28,marker.y*28);
+	
 	for(var i=1; i<5; i++)
 	{	
-		distance[i] = Phaser.Point.distance(target,directions[i]);
+		if(directions[i].index === 1 && i !== blinky.direction)
+			distance[i] = Phaser.Point.distance(target,directions[i]);
+		else
+			distance[i] = 2000;
 	}
-	distance[blinky.direction] = 200;
-	console.log(distance);
 	
 	var smallest = 1;
-	for(i =2; i<5; i++)
+	for(i=2; i<5; i++)
 	{
-		if(distance[smallest] > distance[i]) smallest = i;
+		if(distance[smallest] > distance[i])
+			smallest = i;
 	}
-	
-	if(smallest === 1)
+	console.log(distance);
+	console.log(smallest);
+	if(smallest != blinky.direction && directions[smallest].index === 1)
 	{
-		blinky.body.velocity.x = 0
-		blinky.body.velocity.y = -100;
-		blinky.direction = 3;
-	}
-	else if(smallest === 2)
-	{
-		blinky.direction = 4;
-		blinky.body.velocity.x = -100;
-		blinky.body.velocity.y = 0
-	}
-	else if(smallest === 3)
-	{
-		blinky.direction = 1;
-		blinky.body.velocity.y = 100;
-		blinky.body.velocity.x = 0
-	}
-	else if(smallest === 4)
-	{
-		blinky.direction = 2;
-		blinky.body.velocity.y = 0
-		blinky.body.velocity.x = 100;
+		if(smallest === 1)
+		{
+			blinky.body.velocity.x = 0;
+			blinky.body.velocity.y = -93;
+			blinky.direction = 3;
+		}
+		else if(smallest === 2)
+		{
+			blinky.direction = 4;
+			blinky.body.velocity.x = -93;
+			blinky.body.velocity.y = 0;
+		}
+		else if(smallest === 3)
+		{
+			blinky.direction = 1;
+			blinky.body.velocity.y = 93;
+			blinky.body.velocity.x = 0;
+		}
+		else if(smallest === 4)
+		{
+			blinky.direction = 2;
+			blinky.body.velocity.y = 0;
+			blinky.body.velocity.x = 93;
+		}
 	}
 }
 
